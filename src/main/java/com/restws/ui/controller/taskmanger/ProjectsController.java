bd915e37992632847e92057eb52edf7e577ddffd
kulbirdev <kulbirfamily@gmail.com>
2020-05-28 21:45:28 -0400

    projects controller and Cors

--- src/main/java/com/restws/ui/controller/taskmanger/ProjectsController.java
+++ src/main/java/com/restws/ui/controller/taskmanger/ProjectsController.java
@@ -4,12 +4,13 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.restws.com.restws.ui.model.taskmanager.Project;
-
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 @RestController
 @RequestMapping("api/projects")
 public class ProjectsController {
